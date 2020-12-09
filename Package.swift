b06eb6052a3552e02b901c391b0905fccc752a66
Yannik Ehlert <yannik.ehlert@claas.com>
2020-12-09 18:41:01 +0100

    SendBirdCalls 1.4.0

--- Package.swift
+++ Package.swift
@@ -16,6 +16,6 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        .binaryTarget(name: "SendBirdCalls", url: "https://github.com", checksum: "0000000000000000000000000000000000000000000000000000000000000000"),
+        .binaryTarget(name: "SendBirdCalls", url: "https://github.com/sendbird/sendbird-calls-ios/releases/download/1.4.0/SendBirdCalls.xcframework.zip", checksum: "bd8beafee9443fef64427abf25ebec0c032a0dc76a2668a46b3f9d0cd9484a4f"),
     ]
 )
