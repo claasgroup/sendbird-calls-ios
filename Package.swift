8dc10470b2e1eba55e191051fb826c5ea2f1da9c
Microsoft.TeamFoundation.System <>
2020-12-11 11:22:41 +0100

    SendBirdCalls 1.5.0

--- Package.swift
+++ Package.swift
@@ -16,6 +16,6 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        .binaryTarget(name: "SendBirdCalls", url: "https://github.com/sendbird/sendbird-calls-ios/releases/download/1.4.0/SendBirdCalls.xcframework.zip", checksum: "bd8beafee9443fef64427abf25ebec0c032a0dc76a2668a46b3f9d0cd9484a4f"),
+        .binaryTarget(name: "SendBirdCalls", url: "https://github.com/sendbird/sendbird-calls-ios/releases/download/1.5.0/SendBirdCalls.xcframework.zip", checksum: "e4ed009bb7e0ed0a246546e2fdeecdfabb3bcb7afbf63f11911e10c021effe75"),
     ]
 )
